b0a0cbe975fae1c163fdc3c0ff22588c552b6a02
Romil003 <romilghadge33@gmail.com>
2023-01-03 14:08:56 +0530

    Added : validating input username

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.regex.Pattern;
 
 
 @WebServlet(
@@ -20,20 +21,29 @@ import java.io.PrintWriter;
 public class LoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //defining regex for username
+        String userName = "^[A-Z][a-z]{3,}$";
         //get request parameters for userID and password
         String user = request.getParameter("user");
         String pwd = request.getParameter("pwd");
         //get servlet config init params
         String userID = getServletConfig().getInitParameter("user");
         String password = getServletConfig().getInitParameter("password");
-        if(userID.equals(user) && password.equals(pwd)) {
-            request.setAttribute("user",user);
-            request.getRequestDispatcher("LoginSuccess.jsp").forward(request,response);
+        if(Pattern.matches(userName , user)) {
+            if (userID.equals(user) && password.equals(pwd)) {
+                request.setAttribute("user", user);
+                request.getRequestDispatcher("LoginSuccess.jsp").forward(request, response);
+            } else {
+                RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
+                PrintWriter out = response.getWriter();
+                out.println("<font color=red>Either username or password is incorrect.</font>");
+                rd.include(request, response);
+            }
         } else {
             RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
             PrintWriter out = response.getWriter();
-            out.println("<font color=red>Either username or password is incorrect.</font>");
-            rd.include(request,response);
+            out.println("<font color=red>Either username is invalid.</font>");
+            rd.include(request, response);
         }
     }
 }
